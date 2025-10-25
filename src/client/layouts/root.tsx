8ce23c4929006e0aaf2777e4ecccc247f7164924
meowveloper <meowveloper.dev@gmail.com>
2025-10-25 16:51:51 +0700

    make layout happy

--- src/client/layouts/root.tsx
+++ src/client/layouts/root.tsx
@@ -1,14 +1,22 @@
 import { NavLink, Outlet } from "react-router";
+import {
+    Card,
+    CardDescription,
+} from "@/src/client/components/ui/card"
 
-export default function Root_Layout () {
+export default function Root_Layout() {
     return (
         <div>
             <div className="text-white">
-                <NavLink to="/" end>
-                    Root Layout
-                </NavLink>
+                <Card className="rounded-none px-5">
+                    <CardDescription>
+                        <NavLink to="/" end>
+                            Home
+                        </NavLink>
+                    </CardDescription>
+                </Card>
             </div>
-            <div>
+            <div className="px-5 py-3">
                 <Outlet />
             </div>
         </div>
